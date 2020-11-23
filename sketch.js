facacf3fe2acc3fad2eda83c8407c067d080aec0
alen331 <70333046+alen331@users.noreply.github.com>
2020-11-23 16:52:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,7 +2,9 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var engine,world;
 
 function preload()
 {
@@ -11,23 +13,23 @@ function preload()
 
 function setup() {
 	rectMode(CENTER);
-	createCanvas(800, 700);
+	createCanvas(windowWidth/2,windowHeight/1.5);
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-	bob1 = new BOB(400,550,40);
-	bob2 = new BOB(440,550,40);
-	bob3 = new BOB(360,550,40);
-	bob4 = new BOB(480,550,40);
-	bob5 = new BOB(320,550,40);
-	pend1 = new PEND(400,300,250,30);
-	chain1 = new CHAIN(bob1.body,pend1.body,0,20);
-	chain2 = new CHAIN(bob2.body,pend1.body);
-	chain3 = new CHAIN(bob3.body,pend1.body);
-	chain4 = new CHAIN(bob4.body,pend1.body);
-	chain5 = new CHAIN(bob5.body,pend1.body,{x:40,y:10});
+	pend1 = new Pendulem(150,300,"black");
+	pend2 = new Pendulem(210,300,"black");
+	pend3 = new Pendulem(270,300,"black");
+	pend4 = new Pendulem(330,300,"black");
+	pend5 = new Pendulem(390,300,"black");
+
+	sling1 = new Sling(pend1.body,{x:150,y:100});
+	sling2 = new Sling(pend2.body,{x:210,y:100});
+	sling3 = new Sling(pend3.body,{x:270,y:100});
+	sling4 = new Sling(pend4.body,{x:330,y:100});
+	sling5 = new Sling(pend5.body,{x:390,y:100});
 
 	Engine.run(engine);
   
@@ -36,21 +38,26 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(100);
-  chain1.display();
-  chain2.display();
-  chain3.display();
-  chain4.display();
-  chain5.display();
-  bob1.display();
-  bob2.display();
-  bob3.display();
-  bob4.display();
-  bob5.display();
+  background("black");
+  Engine.update(engine);
+
   pend1.display();
-  drawSprites();
- 
+  pend2.display();
+  pend3.display();
+  pend4.display();
+  pend5.display();
+  
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+  sling5.display();
 }
 
+function keyPressed(){
+  if(keyDown("UP_ARROW")){
+	  Matter.Body.applyForce(pend1.body,pend1.body.position,{x:-0.05,y:-0.05});
+  }
+}
 
 
